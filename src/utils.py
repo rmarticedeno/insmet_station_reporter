67f7ccf9f1ec95f43d1e797d4c202ea3ef5d63b9
rmarticedeno <rmarticedeno@gmail.com>
2025-01-25 23:01:57 -0500

    Use table last modification date instead of the value of the record

--- src/utils.py
+++ src/utils.py
@@ -18,13 +18,19 @@ def PostTelegramMessage(message: str, topic_id: str = None):
 def getConnectionString(db:str):
     return f'DRIVER={{ODBC Driver 18 for SQL Server}};SERVER={ADDR};DATABASE={db};UID={USER};PWD={PASS};TrustServerCertificate=yes'
 
-def checkValidityMaxandMin(row):
+def checkValidity(cursor, db, type):
+    sqlcheck = f"""SELECT modify_date
+    FROM "{db}"."sys"."objects"
+    WHERE "type" IN ('P', 'U', 'V', 'TR', 'FN', 'TF', 'IF')
+    AND name = '{type}';"""
+
+    cursor.execute(sqlcheck)
+    value = cursor.fetchall()[0]
+    
     offset = datetime.timedelta(hours=5)
-    difference = datetime.datetime.utcnow() - offset - row.CurrentTime
+    difference = datetime.datetime.utcnow() - offset - value
 
     return difference.seconds < 60
-
-def checkValidityforMean(row):
     offset = datetime.timedelta(hours=5)
     difference = datetime.datetime.utcnow() - offset - row.EndTime
     
@@ -37,14 +43,12 @@ def getReportValues(db: str, topicId: str, query: str, report_name: str, check =
     cursor = conn.cursor()
     cursor.execute(query)
     records = cursor.fetchall()
-    instance = None
     for r in records:
-        instance = r
         report.update_magnitude(r.MagnitudeId, r.Value)
-
-    if check is not None and not check(instance):
-        return
     
+    if check is not None and not check(cursor):
+        return
+
     PostTelegramMessage(str(report), topicId)
 
 def getMaxValueReport(db: str, topicId: str):
@@ -57,7 +61,7 @@ WHERE CurrentTime = (
     FROM dbo.MaxValue
 )
 """
-    getReportValues(db, topicId, query, 'Valores Máximos', checkValidityMaxandMin)
+    getReportValues(db, topicId, query, 'Valores Máximos', lambda x: checkValidity(x, db, 'MaxValue'))
 
 def getMinValueReport(db: str, topicId: str):
     query = """
@@ -68,7 +72,7 @@ WHERE CurrentTime = (
     FROM dbo.MinValue
 )
 """
-    getReportValues(db, topicId, query, 'Valores Mínimos', checkValidityMaxandMin)
+    getReportValues(db, topicId, query, 'Valores Mínimos', lambda x: checkValidity(x, db, 'MinValue'))
 
 def getMeanValueReport(db: str, topicId: str):
 
@@ -80,4 +84,4 @@ WHERE EndTime = (
     FROM dbo.MeanValue
 )
 """
-    getReportValues(db, topicId, query, 'Valores Promedio', checkValidityforMean)
+    getReportValues(db, topicId, query, 'Valores Promedio', lambda x: checkValidity(x, db, 'MeanValue'))
